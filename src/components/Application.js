e56b7e111641d4fa773250ebf467fc7b21b71038
Théo Malaper <theomalaper.cognez@gmail.com>
2019-10-01 21:14:07 -0400

    selectors implementation

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment"
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -75,12 +58,25 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([])
+
   const appointmentList = appointments.map(appointment => {
     return <Appointment
       key={appointment.id}
       {...appointment}
     />
   })
+
+  useEffect(() => {
+    axios
+      .get("/api/days")
+      .then(response => {
+        setDays(response.data)
+      })
+      .catch(err => {
+        console.log(err.stack)
+      })
+  }, [])
 
   return (
     <main className="layout">
---
+++ src/helpers/selectors.js
@@ -0,0 +1,10 @@
+export function getAppointmentsForDay(state, day) {
+  if (state.days.length === 0) {
+    return []
+  }
+  const filteredDay = state.days.filter(d => d.name === day)
+  if (filteredDay.length === 0) {
+    return []
+  }
+  return filteredDay[0].appointments.map(a => state.appointments[a])
+} 
